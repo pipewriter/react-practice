6374b64b8e7ce74629187caf81caf1b25da42368
Parker <parker@example.com>
2018-07-03 00:25:04 -0400

    checkpoint

--- src/components/BudgetPage/PurchaseHistory.js
+++ src/components/BudgetPage/PurchaseHistory.js
@@ -3,26 +3,43 @@ import DateDisplay from '../Common/DateDisplay';
 import ItemListing from './ItemListing';
 import { sortItemsIntoWeeks } from '../../utils/items';
 
-let Week = (props) => (
-    <div>
-        <div>
-            <span>Week of <DateDisplay time={props.week.startDate} /><button>-</button></span>
-        </div>
-        <div>
-            {props.week.items.map((item) => (
-                <ItemListing key={item.id} item={item} />
-            ))}
-        </div>
-    </div>
-)
+class Week extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {expanded: props.index === 0};
+    }    
+    render(){
+        let toggleExpand = () =>{
+            this.setState({expanded: !this.state.expanded})
+        }
+        let buttonContent;
+        if(this.state.expanded){
+            buttonContent = '-';
+        }else{
+            buttonContent = '+ ' + this.props.week.items.length;
+        }
+        return(
+            <div>
+                <div>
+                    <span>Week of <DateDisplay time={this.props.week.startDate} /><button onClick={toggleExpand}>{buttonContent}</button></span>
+                </div>
+                <div>
+                    {this.props.week.items.map((item) => (
+                        <ItemListing key={item.id} item={item} />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
 
 export default (props) => {
     let weeks = sortItemsIntoWeeks(props.items);
     console.log(weeks);
     return (
         <div>
-            {weeks.map((week) => (
-                <Week key={week.key} week={week} />
+            {weeks.map((week, index) => (
+                <Week key={week.key} index={index} week={week} />
             ))}
         </div>
     )
